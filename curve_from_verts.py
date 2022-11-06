db9bed2f9d695afc0c6746aff1bd390c3059a87b
Msicuro <msicuro33@gmail.com>
2022-11-06 10:11:45 -0500

    Added error if selected items aren't vertices

--- curve_from_verts.py
+++ curve_from_verts.py
@@ -1,6 +1,7 @@
 import pymel.core as pm
 
 def curveFromVerts():
+    #TODO: Change function to create a curve base on positions from anything
     """
     Creates a curve with positions from selected vertices
     """
@@ -9,6 +10,14 @@ def curveFromVerts():
     else:
         raise RuntimeError("No vertices selected!")
 
-    point_positions = [pm.pointPosition(v, w=True) for v in verts]
+    # point_positions = [pm.pointPosition(v, w=True) for v in verts]
 
+    point_positions = []
+    for v in verts:
+        if isinstance(v, pm.MeshVertex):
+            point_positions.append(pm.pointPosition(v, w=True))
+        else:
+            raise RuntimeError('{} is not a vertice! Please only select vertices'.format(v))
+
+    print ("They're all vertices!")
     pm.curve(p=point_positions)
