e3fb5e7688dd84b2c851164cbfb1b37f387ec6c4
Msicuro <msicuro33@gmail.com>
2022-11-26 18:42:41 -0500

    Removed buggy test for control naming

--- createRope.py
+++ createRope.py
@@ -74,7 +74,8 @@ def createCurve():
     cmds.select(clear=True)
     for i in control_transforms:
         cmds.select(i)
-        control_joints.append(centerJoint(name="{}".format(i.replace("BIND", "CTRL"))))
+        # TODO: Fix the control joint naming
+        control_joints.append(centerJoint(name="{}".format(i.replace("LOC", "CTRL"))))
         positions.append(cmds.xform(i, query=True, translation=True, worldSpace=True))
 
     # Increase the size of the control joints
