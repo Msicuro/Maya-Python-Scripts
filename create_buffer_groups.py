b055e1d769a8ed3cae4e37bbb619ea2660d90e8c
Msicuro <msicuro33@gmail.com>
2023-01-09 23:34:58 -0500

    Fixed weird indenting

--- create_buffer_groups.py
+++ create_buffer_groups.py
@@ -3,16 +3,16 @@ import maya.cmds as cmds
 def createThree(selection = cmds.ls(sl=True)):
 
 	for each in selection:
-	    grp1 = cmds.group(empty=True, n = each + "_NULL_GRP")
-	    cmds.delete(cmds.parentConstraint(each, grp1)[0])
-	    grp2 = cmds.group(empty=True, n = each + "_ZERO_GRP")
-	    cmds.delete(cmds.parentConstraint(each, grp2)[0])
-	    grp3 = cmds.group(empty=True, n = each + "_BUFF_GRP")
-	    cmds.delete(cmds.parentConstraint(each, grp3)[0])
-	    cmds.parent(each, grp3)
-	    cmds.parent(grp3, grp2)
-	    cmds.parent(grp2, grp1)
-	    cmds.makeIdentity(each, t=1, r=1,s=1, apply=True)
+		grp1 = cmds.group(empty=True, n = each + "_NULL_GRP")
+		cmds.delete(cmds.parentConstraint(each, grp1)[0])
+		grp2 = cmds.group(empty=True, n = each + "_ZERO_GRP")
+		cmds.delete(cmds.parentConstraint(each, grp2)[0])
+		grp3 = cmds.group(empty=True, n = each + "_BUFF_GRP")
+		cmds.delete(cmds.parentConstraint(each, grp3)[0])
+		cmds.parent(each, grp3)
+		cmds.parent(grp3, grp2)
+		cmds.parent(grp2, grp1)
+		cmds.makeIdentity(each, t=1, r=1,s=1, apply=True)
 
 def createTwo(selection = cmds.ls(sl=True)):
 
