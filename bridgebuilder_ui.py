c994da030ce01373f460f457969fc4dd54ff5cd1
msicuro <msicuro33@gmail.com>
2023-10-23 15:59:36 -0400

    Added "self" to widgets and functions for each rope feature

--- bridgebuilder_ui.py
+++ bridgebuilder_ui.py
@@ -5,14 +5,6 @@ import bridgeBuilder
 
 class RopeUI(QtWidgets.QDialog):
 
-    rope_functions = {
-        "Select Spans": bridgeBuilder.selectSpans,
-        "Create Curve": bridgeBuilder.createCurve,
-        "Set Position Percentage": bridgeBuilder.setPositionPercentage,
-        "Attach To Motion Path": bridgeBuilder.attachToMotionPath,
-        "Bind": bridgeBuilder.bindJoints
-    }
-
     def __init__(self):
         super(RopeUI, self).__init__()
 
@@ -53,11 +45,11 @@ class RopeUI(QtWidgets.QDialog):
         select_spans_layout = QtWidgets.QGridLayout(select_spans_widget)
         main_layout.addWidget(select_spans_widget)
 
-        select_checkbox = QtWidgets.QCheckBox()
-        select_checkbox.setText("Create Joints Along Cylinder")
-        #select_checkbox.setFont(QtGui.QFont( 12))
+        self.select_checkbox = QtWidgets.QCheckBox()
+        self.select_checkbox.setText("Create Joints Along Cylinder")
+        #self.select_checkbox.setFont(QtGui.QFont( 12))
 
-        select_spans_layout.addWidget(select_checkbox, 0, 0)
+        select_spans_layout.addWidget(self.select_checkbox, 0, 0)
 
         select_verts_text = QtWidgets.QLabel("Subdivision Axis: ")
         #select_verts_text.setAlignment(QtCore.Qt.Alignment(1))
@@ -71,20 +63,20 @@ class RopeUI(QtWidgets.QDialog):
         curve_layout = QtWidgets.QGridLayout(curve_widget)
         main_layout.addWidget(curve_widget)
 
-        curve_checkbox = QtWidgets.QCheckBox()
-        curve_checkbox.setText("Create Control Curve")
+        self.curve_checkbox = QtWidgets.QCheckBox()
+        self.curve_checkbox.setText("Create Control Curve")
 
-        curve_layout.addWidget(curve_checkbox, 0, 0)
+        curve_layout.addWidget(self.curve_checkbox, 0, 0)
 
         # Position Percentage section
         position_percent_widget = QtWidgets.QWidget()
         position_percent_layout = QtWidgets.QGridLayout(position_percent_widget)
         main_layout.addWidget(position_percent_widget)
 
-        position_percent_checkbox = QtWidgets.QCheckBox()
-        position_percent_checkbox.setText("Set Locator Curve Position Percentage")
+        self.position_percent_checkbox = QtWidgets.QCheckBox()
+        self.position_percent_checkbox.setText("Set Locator Curve Position Percentage")
 
-        position_percent_layout.addWidget(position_percent_checkbox, 0, 0)
+        position_percent_layout.addWidget(self.position_percent_checkbox, 0, 0)
 
         # Attach to Motion Path section
         motion_path_widget = QtWidgets.QWidget()
@@ -92,13 +84,13 @@ class RopeUI(QtWidgets.QDialog):
         motion_path_widget.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         main_layout.addWidget(motion_path_widget)
 
-        motion_path_checkbox = QtWidgets.QCheckBox()
-        motion_path_checkbox.setText("Attach Locators to Motion Path")
-        rotations_checkbox = QtWidgets.QCheckBox()
-        rotations_checkbox.setText("Include Motion Path Rotations")
+        self.motion_path_checkbox = QtWidgets.QCheckBox()
+        self.motion_path_checkbox.setText("Attach Locators to Motion Path")
+        self.rotations_checkbox = QtWidgets.QCheckBox()
+        self.rotations_checkbox.setText("Include Motion Path Rotations")
 
-        motion_path_layout.addWidget(motion_path_checkbox, 0, 0)
-        motion_path_layout.addWidget(rotations_checkbox, 1, 0, 1, 2, alignment=QtCore.Qt.AlignCenter)
+        motion_path_layout.addWidget(self.motion_path_checkbox, 0, 0)
+        motion_path_layout.addWidget(self.rotations_checkbox, 1, 0, 1, 2, alignment=QtCore.Qt.AlignCenter)
 
 
         # Bind section
@@ -106,13 +98,13 @@ class RopeUI(QtWidgets.QDialog):
         bind_layout = QtWidgets.QGridLayout(bind_widget)
         main_layout.addWidget(bind_widget)
 
-        bind_curve_checkbox = QtWidgets.QCheckBox()
-        bind_curve_checkbox.setText("Bind Curve to Control Joints")
-        bind_mesh_checkbox = QtWidgets.QCheckBox()
-        bind_mesh_checkbox.setText("Bind Mesh to Locator Joints")
+        self.bind_curve_checkbox = QtWidgets.QCheckBox()
+        self.bind_curve_checkbox.setText("Bind Curve to Control Joints")
+        self.bind_mesh_checkbox = QtWidgets.QCheckBox()
+        self.bind_mesh_checkbox.setText("Bind Mesh to Locator Joints")
 
-        bind_layout.addWidget(bind_curve_checkbox, 0, 0)
-        bind_layout.addWidget(bind_mesh_checkbox, 1, 0)
+        bind_layout.addWidget(self.bind_curve_checkbox, 0, 0)
+        bind_layout.addWidget(self.bind_mesh_checkbox, 1, 0)
 
         # Run Button
         run_button = QtWidgets.QPushButton("Run")
@@ -123,8 +115,8 @@ class RopeUI(QtWidgets.QDialog):
         select_spans_widget.setStyleSheet("font-size: {}px".format(main_font_size))
         curve_widget.setStyleSheet("font-size: {}px".format(main_font_size))
         position_percent_widget.setStyleSheet("font-size: {}px".format(main_font_size))
-        motion_path_checkbox.setStyleSheet("font-size: {}px".format(main_font_size))
-        rotations_checkbox.setStyleSheet("font-size: 12px")
+        self.motion_path_checkbox.setStyleSheet("font-size: {}px".format(main_font_size))
+        self.rotations_checkbox.setStyleSheet("font-size: 12px")
         bind_widget.setStyleSheet("font-size: {}px".format(main_font_size))
         run_button.setStyleSheet("font-size: {}px".format(main_font_size))
 
@@ -135,20 +127,66 @@ class RopeUI(QtWidgets.QDialog):
         #     self.type_combo.currentText()))))
 
         # Test selectSpans functionality
-        run_button.clicked.connect(self.runSelectSpans)
+        run_button.clicked.connect(self.runButtonFunctions())
 
     def runSelectSpans(self):
-        self.bind_joints, \
-        self.locators, \
-        self.spans, \
-        self.name, \
-        self.mesh, \
-        self.constructor = bridgeBuilder.selectSpans("{}_{}_{}".format(
-            self.name_combo.currentText(),
-            self.name_line.text(),
-            self.type_combo.currentText()
+        self.bind_joints,\
+        self.locators,\
+        self.spans,\
+        self.name,\
+        self.mesh,\
+        self.constructor = bridgeBuilder.selectSpans("{}_{}_{}".format(self.name_combo.currentText(),
+                                                                       self.name_line.text(),
+                                                                       self.type_combo.currentText()
         ))
         return self.bind_joints, self.locators, self.spans, self.name, self.mesh, self.constructor
+
+    def runCreateCurve(self):
+        self.curv, \
+        self.locators, \
+        self.ctrl_joints = bridgeBuilder.createCurve("{}_{}_{}".format(self.name_combo.currentText(),
+                                                                       self.name_line.text(),
+                                                                       self.type_combo.currentText()))
+
+    def runSetPositionPercentage(self):
+        self.joint_percentage = bridgeBuilder.setPositionPercentage(self.curv, self.locators)
+
+    def runAttachMotionPaths(self):
+        self.motion_paths = bridgeBuilder.attachToMotionPath(self.joint_percentage,
+                                                             self.curv, self.locators,
+                                                             ctrl_joints=self.ctrl_joints,
+                                                             rope_type=self.type_combo.currentText())
+    def runBindJoints(self):
+        if self.bind_curve_checkbox.isChecked() == True and self.bind_mesh_checkbox.isChecked() == False:
+            bridgeBuilder.bindJoints(self.mesh, self.bind_joints)
+        elif self.bind_mesh_checkbox.isChecked() == True and self.bind_curve_checkbox.isChecked() == False:
+            bridgeBuilder.bindJoints(self.curv, self.ctrl_joints)
+        elif self.bind_curve_checkbox.isChecked() == True and self.bind_mesh_checkbox.isChecked() == True:
+            bridgeBuilder.bindJoints(self.mesh, self.bind_joints)
+            bridgeBuilder.bindJoints(self.curv, self.ctrl_joints)
+
+    def checkCheckBoxes(self):
+        rope_functions = {
+            self.select_checkbox: self.runSelectSpans,
+            self.curve_checkbox: self.runCreateCurve,
+            self.position_percent_checkbox: self.runSetPositionPercentage,
+            self.motion_path_checkbox: self.runAttachMotionPaths,
+            self.bind_curve_checkbox: self.runBindJoints,
+            self.bind_mesh_checkbox: self.runBindJoints
+        }
+
+        self.button_functions = []
+
+        for i in rope_functions:
+            if rope_functions[i].isChecked() == True:
+                self.button_functions.append(rope_functions[i])
+        return self.rope_functions
+
+    def runButtonFunctions(self):
+        for i in self.button_functions:
+            self.button_functions[i]()
+
+
 
 
 
