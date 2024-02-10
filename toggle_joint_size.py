e166669749ae9b2c38e51f4d7f4934890159813c
msicuro <msicuro33@gmail.com>
2024-02-09 19:01:17 -0500

    Removed itertools.cycle from joint sizing function
    Redid the function so that it can be reloaded in Maya instead of relying on the same instance of Cycle each session to loop through the joint sizes list

--- toggle_joint_size.py
+++ toggle_joint_size.py
@@ -1,9 +1,22 @@
 import pymel.core as pm
 from itertools import cycle
 
-display_sizes = [0.01, 0.05, 0.1, 0.2, 0.5, 0.8, 1.0, 1.5]
-myIterator = cycle(display_sizes)
+# myIterator = cycle(display_sizes)
 
 def setJointSize():
-    new_display = next(myIterator)
-    pm.jointDisplayScale(new_display)
+    '''
+    Cycles through joint display sizes
+    Returns:
+
+    '''
+    # new_display = next(myIterator)
+    # pm.jointDisplayScale(new_display)
+    display_sizes = [0.01, 0.05, 0.1, 0.2, 0.5, 0.8, 1.0, 1.5]
+
+    current_scale = pm.jointDisplayScale(query=True)
+
+    if current_scale not in display_sizes or current_scale == display_sizes[-1]:
+        pm.jointDisplayScale(display_sizes[0])
+    else:
+        current_scale_index = display_sizes.index(current_scale)
+        pm.jointDisplayScale(display_sizes[current_scale_index + 1])
